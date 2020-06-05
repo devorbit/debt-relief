f2f75291fb99a93c80d3c7987d3a414b6806e1e4
Teja Rachakonda <Teja.Rachakonda@experian.com>
2020-06-05 12:08:17 +0530

    Dropdowns Added

--- DR-Client/src/app/criteria/criteria.component.ts
+++ DR-Client/src/app/criteria/criteria.component.ts
@@ -6,7 +6,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { user_profile } from '../user.profile';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from '../../environments/environment';
-import {DebtReliefOption} from '../models/debt-relief-option';
+import { DebtReliefOption } from '../models/debt-relief-option';
 
 @Component({
   selector: 'criteria',
@@ -23,7 +23,7 @@ export class CriteriaComponent implements OnInit {
   uploader: FileUploader;
   first = true;
   tradeData;
-  displayedColumns: string[] = ['accountNB', 'acctSTATUSCD', 'acctTypeCD', 'acctBalanceAm', 'acctPaymentAmount', 'subscriberName', 'enhancedSpclCmntCD', 'termsFreq', 'terms', 'debtReliefOption', 'debtReliefValue', 'apply'];
+  displayedColumns: string[] = ['accountNB', 'acctSTATUSCD', 'acctTypeCD', 'acctBalanceAm', 'acctPaymentAmount', 'subscriberName', 'enhancedSpclCmntCD', 'termsFreq', 'terms', 'debtReliefOption', 'apply'];
   debtReliefOptionList: DebtReliefOption;
 
   constructor(private formBuilder: FormBuilder, private criteriaService: CriteriaService, private spinner: NgxSpinnerService) {
@@ -108,46 +108,71 @@ export class CriteriaComponent implements OnInit {
   nextClicked() {
     this.spinner.show();
     const criteria = new Criteria(Number(user_profile.pin), this.reasonDD, this.needDD);
-    this.criteriaService.submitCriteria(criteria).subscribe(
+    /* this.criteriaService.submitCriteria(criteria).subscribe(
       criteriaData => {
-        console.log('Criteria Data', criteriaData);
-        this.criteriaService.getTrade(user_profile.pin).subscribe(
-          tradeData => {
-            this.first = false;
-            // let tradeData = [[{ "_id": { "$oid": "5ed7ac6ee2ca713a0b1265fd" }, "pin": { "$long": 1234 }, "accountNB": { "$long": 1234567 }, "acctSTATUSCD": "11", "acctOpenDT": "02-02-2001", "acctTypeCD": "01", "acctBalanceAm": { "$long": 5000 }, "acctPaymentAmount": { "$long": 2000 }, "subscriberId": "0001", "subscriberName": "Test Bank", "enhancedSpclCmntCD": "00", "termsFreq": "D", "terms": 20 }, { "_id": { "$oid": "5ed7ac6ee2ca713a0b1265fd" }, "pin": { "$long": 1234 }, "accountNB": { "$long": 1234567 }, "acctSTATUSCD": "11", "acctOpenDT": "02-02-2001", "acctTypeCD": "01", "acctBalanceAm": { "$long": 5000 }, "acctPaymentAmount": { "$long": 2000 }, "subscriberId": "0001", "subscriberName": "Test Bank", "enhancedSpclCmntCD": "00", "termsFreq": "D", "terms": 20 }]];
-            const tempTradeData = tradeData[0];
-            console.log('Trade Data', tradeData);
-            console.log('Temp Trade Data', tempTradeData);
-            for (const item of tempTradeData) {
-              this.criteriaService.getReliefValue(item.subscriberId, item.acctTypeCD, user_profile.score).subscribe(
-
-                      (reliefData: any) => {
-                  this.debtReliefOptionList = reliefData;
-                  // let reliefData = [[{ "_id": { "$oid": "5ed71fe38221ebeace230c6d" }, "subscriberId": "12345", "creditScoreFrom": 300, "creditScoreTo": 700, "debtReliefOption": "DP", "debtReliefValue": 90, "loanType": "01" }]];
-                  const tempReliefData = reliefData[0][0];
-                  console.log('Relief Data', tempReliefData);
-                  item.debtReliefOption = tempReliefData.debtReliefOption;
-                  item.debtReliefValue = tempReliefData.debtReliefValue;
-                  console.log('Temp Trade Data', tempTradeData);
-                  this.tradeData = tempTradeData;
-                }, err => {
-                  console.log('Error', err);
-                  this.spinner.hide();
-                }
-              );
-            }
-            this.tradeData = tempTradeData;
-            this.spinner.hide();
-          }, err => {
-            console.log('Error ', err);
-            this.spinner.hide();
-          }
-        );
-      }, err => {
+        console.log('Criteria Data', criteriaData); */
+    /* this.criteriaService.getTrade(user_profile.pin).subscribe(
+      tradeData => { */
+    this.first = false;
+    let tradeData = [[{ "_id": { "$oid": "5ed7ac6ee2ca713a0b1265fd" }, "pin": { "$long": 1234 }, "accountNB": { "$long": 1234567 }, "acctSTATUSCD": "11", "acctOpenDT": "02-02-2001", "acctTypeCD": "00", "acctBalanceAm": { "$long": 5000 }, "acctPaymentAmount": { "$long": 2000 }, "subscriberId": "0001", "subscriberName": "Test Bank", "enhancedSpclCmntCD": "00", "termsFreq": "D", "terms": 20 }, { "_id": { "$oid": "5ed7ac6ee2ca713a0b1265fd" }, "pin": { "$long": 1234 }, "accountNB": { "$long": 1234567 }, "acctSTATUSCD": "11", "acctOpenDT": "02-02-2001", "acctTypeCD": "12", "acctBalanceAm": { "$long": 5000 }, "acctPaymentAmount": { "$long": 2000 }, "subscriberId": "0001", "subscriberName": "Test Bank", "enhancedSpclCmntCD": "00", "termsFreq": "D", "terms": 20 }]];
+    const tempTradeData = tradeData[0];
+    console.log('Trade Data', tradeData);
+    console.log('Temp Trade Data', tempTradeData);
+    for (const item of tempTradeData) {
+      /* this.criteriaService.getReliefValue(item.subscriberId, item.acctTypeCD, user_profile.score).subscribe(
+              (reliefData: any) => { */
+      let reliefData = [{ "_id": { "$oid": "5ed8e86f8221ebeace238d1f" }, "subscriberId": "0001", "creditScoreFrom": 600, "creditScoreTo": 850, "debtReliefOption": "DP", "debtReliefValue": 90, "loanType": "00" }, { "_id": { "$oid": "5ed8eac18221ebeace238df4" }, "subscriberId": "0001", "creditScoreFrom": 600, "creditScoreTo": 800, "debtReliefOption": "RI", "debtReliefValue": 0.2, "loanType": "00" }];
+      console.log('Relief Data', reliefData);
+      let debtReliefArray = [];
+      for (const relief of reliefData) {
+        let displayVal ='';
+        switch (relief.debtReliefOption) {
+          case 'DP':
+            displayVal = 'Deferred Payments';
+            break;
+          case 'RT':
+            displayVal = 'Refinance with longer repayment terms';
+            break;
+          case 'RI':
+            displayVal = 'Reduce interest rates';
+            break;
+          case 'RP':
+            displayVal = 'Reduce payment amount';
+            break;
+          case 'WF':
+            displayVal = 'Waived late fees';
+            break;
+          default:
+            break;
+        }
+        displayVal = relief.debtReliefValue + ' - ' + displayVal;
+        const arrayItem = {
+         'debtReliefOption': relief.debtReliefOption,
+         'debtReliefDisplayVal': displayVal
+        }
+        debtReliefArray.push(arrayItem);
+      }
+      item['debtReliefArray'] = debtReliefArray;
+      console.log('Temp Trade Data', tempTradeData);
+      this.tradeData = tempTradeData;
+      /* }, err => {
         console.log('Error', err);
         this.spinner.hide();
       }
-    );
+    ); */
+    }
+    this.tradeData = tempTradeData;
+    this.spinner.hide();
+    /*  }, err => {
+       console.log('Error ', err);
+       this.spinner.hide();
+     }
+   );
+ }, err => {
+   console.log('Error', err);
+   this.spinner.hide();
+ }
+); */
   }
 
   applyClicked(rowJson) {
